826c93db7631d8a1235612ec1c00ae6efeb21142
Ashifa <mdashifa12@gmail.com>
2026-05-05 22:59:28 +0530

    Added keyword-based skill detection

--- analyzer/views.py
+++ analyzer/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 import os
 from django.conf import settings
 import PyPDF2
+import re
 
 def home(request):
     if request.method == 'POST':
         uploaded_file = request.FILES.get('resume')
+        
         if uploaded_file:
             file_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
-            
+
             with open(file_path, 'wb+') as destination:
                 for chunk in uploaded_file.chunks():
                     destination.write(chunk)
@@ -18,11 +20,37 @@ def home(request):
             with open(file_path, 'rb') as file:
                 reader = PyPDF2.PdfReader(file)
                 for page in reader.pages:
-                    text += page.extract_text()
+                    extracted = page.extract_text()
+                    if extracted:
+                        text += extracted
+
+            # Clean text
+            text = text.replace('\n', ' ')
+            text = re.sub(r'\s+', ' ', text)
+            text = re.sub(r'\s*-\s*', '-', text)
+
+            # Skill list
+            skills_list = [
+                'python', 'java', 'c++', 'django', 'react', 'node.js',
+                'machine learning', 'deep learning', 'nlp',
+                'data structures', 'algorithms',
+                'sql', 'mongodb', 'fastapi', 'docker'
+            ]
+
+            # Skill detection
+            detected_skills = []
+            text_lower = text.lower()
+
+            for skill in skills_list:
+                if skill in text_lower:
+                    detected_skills.append(skill)
+
+            preview_text = text[:1000]
 
             return render(request, 'home.html', {
                 'message': 'File uploaded successfully',
-                'text': text
+                'text': preview_text,
+                'skills': detected_skills
             })
 
     return render(request, 'home.html')
